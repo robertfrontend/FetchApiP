24bed05e325fccf9d1c3a973eed7e564025f1b5d
Robert Romero Medina <enanow41romero@gmail.com>
2020-05-10 21:58:39 -0400

    agregando load y agregando limite de imagenes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-const btnEmpleados = document.getElementById('btnEmpleados')
-const btnClientes = document.getElementById('btnClientes')
+const btnEmpleados = document.getElementById('btnEmpleados');
+const btnClientes = document.getElementById('btnClientes');
 
-btnEmpleados.addEventListener('click', mostrarEmpleados)
-btnClientes.addEventListener('click', mostrarClientes)
+btnEmpleados.addEventListener('click', mostrarEmpleados);
+btnClientes.addEventListener('click', mostrarClientes);
 
 
 var carga = `
@@ -15,36 +15,35 @@ function mostrarEmpleados() {
             return respuesta.json();
         })
         .then(function(data){
-           document.querySelector('.divCarga').innerHTML = carga
 
-            setTimeout(function(){
-                let html = ''; 
-                data.forEach(function(empleado){
-                    html += `
-                        <div class="card tarjeta  border-info mb-3" style="max-width: 40rem;">
-                            <div class="card-header">
-                                <h3>Empleados</h3>
-                            </div>
-                            <div class="card-body">
-                                <p><span>Nombre:</span> ${empleado.nombre}</p>
-                                <p><span>Apellido:</span> ${empleado.apellido} </p>
-                                <p><span>Correo:</span> ${empleado.correo} </p>
-                                <p><span>Puesto:</span> ${empleado.puesto} </p>
-                            </div>
+            let html = ''; 
+            data.forEach(function(empleado){
+                html += `
+                    <div class="card tarjeta  border-info mb-3" style="max-width: 40rem;">
+                        <div class="card-header">
+                            <h3>Empleados</h3>
                         </div>
-                    `;
-                })
-                document.querySelector('.resultados').innerHTML = html
-                document.querySelector('.divCarga').innerHTML = ''
-
-            },1000)
+                        <div class="card-body">
+                            <p><span>Nombre:</span> ${empleado.nombre}</p>
+                            <p><span>Apellido:</span> ${empleado.apellido} </p>
+                            <p><span>Correo:</span> ${empleado.correo} </p>
+                            <p><span>Puesto:</span> ${empleado.puesto} </p>
+                        </div>
+                    </div>
+                `;
+            })
+            document.querySelector('.resultados').innerHTML = html
+            
         })
         .catch(function(error){
             console.log(error)
         });
+        
 }
 
+
 function mostrarClientes() {
+    document.querySelector('.divCarga').innerHTML = carga;
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(function(respuesta){
             return respuesta.json();
@@ -52,31 +51,28 @@ function mostrarClientes() {
         .then(function(data){
            document.querySelector('.divCarga').innerHTML = carga
 
-            setTimeout(function(){
-                let html = ''; 
-                data.forEach(function(users){
-                    html += `
-                        <div class="card tarjeta  border-success mb-3" style="max-width: 40rem;">
-                            <div class="card-header">
-                                <h3>Clientes</h3>
-                            </div>
-                            <div class="card-body">
-                                <p><span>Nombre:</span> ${users.name}</p>
-                                <p><span>Correo:</span> ${users.email} </p>
-                                <p><span>Ciudad:</span> ${users.address.city} </p>
-                                <p><span>Compañia:</span> ${users.company.name}  </p>
-
-                            </div>
+            let html = ''; 
+            data.forEach(function(users){
+                html += `
+                    <div class="card tarjeta  border-success mb-3" style="max-width: 40rem;">
+                        <div class="card-header">
+                            <h3>Clientes</h3>
                         </div>
-                    `;
-                })
-                document.querySelector('.resultados').innerHTML = html
-                document.querySelector('.divCarga').innerHTML = ''
+                        <div class="card-body">
+                            <p><span>Nombre:</span> ${users.name}</p>
+                            <p><span>Correo:</span> ${users.email} </p>
+                            <p><span>Ciudad:</span> ${users.address.city} </p>
+                            <p><span>Compañia:</span> ${users.company.name}  </p>
+                        </div>
+                    </div>
+                `;
+            })
+            document.querySelector('.resultados').innerHTML = html
+            document.querySelector('.divCarga').innerHTML = ''
 
-            },1000)
         })
         .catch(function(){
-            alert('Hubo un error')
+            console.log('errorrrr')
         });
 }
 
--- js/fotos.js
+++ js/fotos.js
@@ -1,16 +1,59 @@
 
-const linkFotos = 'https://picsum.photos/v2/list'
+const linkFotos = 'https://picsum.photos/v2/list?page=1&limit=20'
 
-fetch(linkFotos)
-.then(respuesta => {
-    return respuesta.json();
-})
-.then(datos => {
-    let miHtml = '' ;
-    datos.forEach(photos => {
+var carga = `
+    <div class="lds-dual-ring"></div>
+`;
 
-        miHtml += `
-            <div class="card tarjeta border-primary ">
+window.onload = () => {
+        document.querySelector('.divCarga').innerHTML = carga
+        fetch(linkFotos)
+        .then(respuesta => {
+            return respuesta.json();
+        })
+        .then(datos => {
+            let miHtml = '' ;
+            datos.forEach(photos => {
+                
+                miHtml += `
+                    <div class="card tarjetaF border-primary ">
+                        <div>
+                            <img class="card-img-top"src="${photos.download_url} " alt="">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title" >
+                                <span class="text-muted" >By:</span> ${photos.author}
+                            </h5>
+                            <a class="btn btn-danger" target="_blank"  href="${photos.url}" >
+                                Descargar
+                            </a>
+                        </div>
+                    </div>
+                `;
+            })
+            document.querySelector('.result').innerHTML = miHtml
+            document.querySelector('.divCarga').innerHTML = ''
+    })
+}
+
+const linkOtras = 'https://picsum.photos/v2/list?page=2&limit=20'
+
+const botonVerMas = document.getElementById('verOtras')
+botonVerMas.addEventListener('click', paginaSigui )
+
+
+function paginaSigui() {
+    document.querySelector('.divCarga').innerHTML = carga
+    fetch(linkOtras)
+    .then(resul => {
+        return resul.json()
+    })
+    .then(data => {
+        let html2 = ''
+
+        data.forEach(photos => {
+            html2 += `
+            <div class="card tarjetaF border-primary ">
                 <div>
                     <img class="card-img-top"src="${photos.download_url} " alt="">
                 </div>
@@ -24,6 +67,9 @@ fetch(linkFotos)
                 </div>
             </div>
         `;
+        })
+        document.querySelector('.result').innerHTML = html2
+        document.querySelector('.divCarga').innerHTML = ''
     })
-    document.querySelector('.result').innerHTML = miHtml
-})
+}
+
