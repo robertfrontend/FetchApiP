b73bf325a44cbdb456e6035d70e4bd8434604692
Robert Romero Medina <enanow41romero@gmail.com>
2020-06-23 15:01:21 -0400

    solucionando error en api fotos

--- js/app.js
+++ js/app.js
@@ -7,56 +7,80 @@ var carga = `
 
 UI.btnEmpleados.addEventListener('click', e => {
     e.preventDefault();
+
     const api = new ApiEmpleados()
+
+    // agregar spiner
+    UI.divCarga.innerHTML = carga
+
     api.consApiEmpleados()
         .then(datos => {
             const res = datos.respuesta
 
             res.forEach(empleado => {
+
+                // destructurar el json
+                const { nombre, apellido, correo, puesto } = empleado;
+
                 UI.divResultados.innerHTML += `
                      <div class="card tarjeta  border-info mb-3" style="max-width: 40rem;">
                          <div class="card-header">
                              <h3>Empleados</h3>
                          </div>
                          <div class="card-body">
-                             <p><span>Nombre:</span> ${empleado.nombre}</p>
-                             <p><span>Apellido:</span> ${empleado.apellido} </p>
-                             <p><span>Correo:</span> ${empleado.correo} </p>
-                             <p><span>Puesto:</span> ${empleado.puesto} </p>
+                             <p><span>Nombre:</span> ${nombre}</p>
+                             <p><span>Apellido:</span> ${apellido} </p>
+                             <p><span>Correo:</span> ${correo} </p>
+                             <p><span>Puesto:</span> ${puesto} </p>
                          </div>
                      </div>
                 `;
             })
+            //quitar el spiner
+            UI.divCarga.innerHTML = ''
+
         })
 } )
 
 UI.btnClientes.addEventListener('click', e => {
     e.preventDefault();
 
-    const api = new ApiClientes()
+    // agregar spiner
+    UI.divCarga.innerHTML = carga;
+
+    const api = new ApiClientes();
+
     api.consApiClientes()
         .then(datos => {
             const res = datos.respuesta
 
             res.forEach(clientes => {
+                // destructurar el json
+                const {  name, email, address, company  } = clientes;
+                
                 UI.divResultados.innerHTML += `
                   <div class="card tarjeta  border-success mb-3" style="max-width: 40rem;">
                       <div class="card-header">
                           <h3>Clientes</h3>
                       </div>
                       <div class="card-body">
-                          <p><span>Nombre:</span> ${clientes.name}</p>
-                          <p><span>Correo:</span> ${clientes.email} </p>
-                          <p><span>Ciudad:</span> ${clientes.address.city} </p>
-                          <p><span>Compañia:</span> ${clientes.company.name}  </p>
+                          <p><span>Nombre:</span> ${name}</p>
+                          <p><span>Correo:</span> ${email} </p>
+                          <p><span>Ciudad:</span> ${address.city} </p>
+                          <p><span>Compañia:</span> ${company.name}  </p>
                       </div>
                   </div>
                 `;
             })
+
+            // quitar spiner
+            UI.divCarga.innerHTML = ''
+
         })
 })
 
-document.getElementById('limpiarTodo').addEventListener('click', borrarTodo)
+document.getElementById('limpiarTodo').
+addEventListener('click', borrarTodo);
 
 function borrarTodo() {
     document.querySelector('.resultados').innerHTML = '';
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,4 @@
 export const btnEmpleados = document.querySelector('#btnEmpleados'),
              btnClientes = document.querySelector('#btnClientes'),
-            divResultados = document.querySelector('.resultados');
+            divResultados = document.querySelector('.resultados'),
+            divCarga = document.querySelector('.divCarga')
